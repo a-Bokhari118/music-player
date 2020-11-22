fefd5e9532a0edf8408b68067a0359f3007956a5
Slade Wilson <a.bokhari118@hotmail.com>
2020-11-22 15:46:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
+import Player from './components/Player';
+import Song from './components/Song';
+import data from './util';
+import './styles/app.scss';
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Song currentSong={currentSong} />
+      <Player />
     </div>
   );
 }
